70ebd45e7e5ce2c6890198fa157cd200de63234e
I-PMAN <heroparth@gmail.com>
2025-06-26 19:39:41 +0530

    Adding the discover section and features section in the landing page.

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import HeroSection from "./HeroSection";
+import FeaturesSection from "./FeaturesSection";
+import DiscoverSection from "./DiscoverSection";
 const Landing = () => {
   return (
     <div>
       <HeroSection />
+      <FeaturesSection />
+      <DiscoverSection />
     </div>
   );
 };
